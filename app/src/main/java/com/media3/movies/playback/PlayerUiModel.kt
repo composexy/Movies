cabb07e417117a0dab750459a394d27784984893
Biking Wang <baiqin.fubotv@gmail.com>
2025-02-06 10:26:45 -0500

    Section 6 lecture 14 - Finished.

--- app/src/main/java/com/media3/movies/playback/PlayerUiModel.kt
+++ app/src/main/java/com/media3/movies/playback/PlayerUiModel.kt
@@ -1,6 +1,7 @@
 package com.media3.movies.playback
 
 import com.media3.movies.R
+import org.intellij.lang.annotations.Language
 
 data class PlayerUiModel(
     val videoAspectRatio: Float = 16.0f / 9.0f,
@@ -20,6 +21,69 @@ data class TimelineUiModel(
     val currentPositionInMs: Long,
     val bufferedPositionInMs: Long
 )
+
+data class TrackSelectionUiModel(
+    val selectedVideoTrack: VideoTrack,
+    val videoTracks: List<VideoTrack>,
+    val selectedAudioTrack: AudioTrack,
+    val audioTracks: List<AudioTrack>,
+    val selectedSubtitleTrack: SubtitleTrack,
+    val subtitleTracks: List<SubtitleTrack>
+)
+
+data class AudioTrack(
+    val language: String
+) {
+    val displayName: String
+        get() {
+            return when (this) {
+                AUTO -> "Auto"
+                NONE -> "None"
+                else -> language
+            }
+        }
+
+    companion object {
+        val AUTO = AudioTrack("Auto")
+        val NONE = AudioTrack("None")
+    }
+}
+
+data class SubtitleTrack(
+    val language: String
+) {
+    val displayName: String
+        get() {
+            return when (this) {
+                AUTO -> "Auto"
+                NONE -> "None"
+                else -> language
+            }
+        }
+
+    companion object {
+        val AUTO = SubtitleTrack("Auto")
+        val NONE = SubtitleTrack("None")
+    }
+}
+
+data class VideoTrack(
+    val width: Int,
+    val height: Int
+) {
+
+    val displayName: String
+        get() {
+            return when (this) {
+                AUTO -> "Auto"
+                else -> "$width $height"
+            }
+        }
+
+    companion object {
+        val AUTO = VideoTrack(0 ,0)
+    }
+}
 
 fun PlaybackState.isReady(): Boolean {
     return this == PlaybackState.PAUSED || this == PlaybackState.PLAYING
